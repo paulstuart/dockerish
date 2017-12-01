d6257f2776711a0c29a3ff976451fc40beb09219
Paul Stuart <pauleyphonic@gmail.com>
2017-12-01 14:10:01 -0800

    updates

--- docker/node/build.sh
+++ docker/node/build.sh
@@ -1,1 +1,8 @@
-docker build -t pstuart/node:4.4 .
+#docker build -t pstuart/node:4.4 .
+docker build \
+	--build-arg http_proxy=$http_proxy 	\
+	--build-arg https_proxy=$https_proxy	\
+	--build-arg HTTP_PROXY=$HTTP_PROXY	\
+	--build-arg HTTPS_PROXY=$HTTPS_PROXY	\
+	--build-arg no_proxy=$no_proxy		\
+	-t pstuart/node:6.11.1 .
